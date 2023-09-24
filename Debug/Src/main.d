efa514b3c625f8085770b9a42185709d49cc1ebf
ThiPham19 <phamhongthi19@gmail.com>
2023-09-24 14:04:51 +0700

    Done

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,12 +1,13 @@
 Src/main.o: ../Src/main.c \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
- C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_exti.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h \
@@ -20,13 +21,14 @@ Src/main.o: ../Src/main.c \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h \
  C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rng.h
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
-C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_exti.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h:
 C:/Users/Dell/STM32CubeIDE/library/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h:
--- Src/main.c
+++ Src/main.c
@@ -13,6 +13,7 @@
 /*                              INCLUDE FILES                                 */
 /******************************************************************************/
 #include "system_stm32f4xx.h"
+#include "stm32f401re_exti.h"
 #include "stm32f401re_rcc.h"
 #include "timer.h"
 #include "eventman.h"
@@ -33,21 +34,35 @@
 /******************************************************************************/
 /*                     PRIVATE TYPES and DEFINITIONS                         */
 /******************************************************************************/
-#define LCD_WIDTH	104
-#define LCD_HEIGHT	104
+#define LCD_WIDTH	127		//127 pixel -> w = 25cell
+#define LCD_HEIGHT	119 	//117 pixel -> h = 23cell
 #define SNAKE_MAX_LNG		(uint16_t)(100)
 #define SNAKE_INIT_LNG		(uint16_t)(3)
 #define INVALID_COORDS		(uint16_t)(-1)
-#define SNAKE_INIT_X_CORD	(uint16_t)(1)
+#define GENERAL_ERROR		(uint16_t)(-1)
+
+#define SNAKE_INIT_X_CORD	(uint16_t)(2)
 #define SNAKE_INIT_Y_CORD	(uint16_t)(10)
 #define SNAKE_WON_LIMIT		(uint16_t)(SNAKE_MAX_LNG - 1)
 
-
 #define ARENA_MAX_X			(uint16_t)(25)
-#define ARENA_MAX_Y			(uint16_t)(23)
-#define ARENA_MIN_X			(uint16_t)(0)
-#define ARENA_MIN_Y			(uint16_t)(0)
+#define ARENA_MIN_X			(int8_t)(-1)
+#define ARENA_MAX_Y			(uint16_t)(22)
+#define ARENA_MIN_Y			(int8_t)(-1)
 
+#define FOOD_MAX_X			(uint16_t)(24)
+#define FOOD_MIN_X			(uint16_t)(0)
+#define FOOD_MAX_Y			(uint16_t)(22)
+#define FOOD_MIN_Y			(uint16_t)(0)
+#define FOOD_MAX_ITER		(uint16_t)(1000)
+
+#define ARENA_OFFSET_X		(uint16_t)(1)
+#define ARENA_OFFSET_Y		(uint16_t)(8)
+#define CELL_SIZE			(uint16_t)(5)
+#define FOOD_SIZE			(uint16_t)(3)
+
+#define SPEED_1				(uint16_t)(100)
+#define SPEED_2				(uint16_t)(500)
 /*	@brief Events APIs*/
 //Định nghĩa kiểu dữ liệu enum cho các sự kiện và trạng thái của chương trình chính
 typedef enum {
@@ -74,7 +89,7 @@ typedef struct snake_tag
 	snake_dir_e direction;
 	coord_t body[SNAKE_MAX_LNG];
 	uint16_t length;
-	coord_t ghost;
+	coord_t ghost;  //bóng của cái đuôi, dùng cho hàm tăng độ dài :vv
 	snake_state_e state;
 } snake_t;
 typedef struct food_tag
@@ -93,19 +108,19 @@ typedef struct food_tag
 /******************************************************************************/
 static ucg_t ucg;
 static ucg_t ucg1;
-uint8_t eCurrentState = 0;
 static uint8_t IdTimer = NO_TIMER;
 static uint16_t gRandSeed;
-char extKeyBoardButton;
+static uint32_t gPrgCycle = 0;
+static char score_display[20] = "";
+static uint8_t score = 0;
 
-
-//static uint8_t score;
-//static char score_display[20] = "";
-//uint32_t gPrgCycle = GetMilSecTick();
+static uint8_t running = 0;
+static uint16_t speed;
 /******************************************************************************/
 /*                              EXPORTED DATA                                 */
 /******************************************************************************/
-
+uint8_t eCurrentState = 0;
+char extKeyBoardButton;
 /******************************************************************************/
 /*                            PRIVATE FUNCTIONS                               */
 /******************************************************************************/
@@ -114,18 +129,30 @@ static state_app_t GetStateApp(void);
 static void SetStateApp(state_app_t state);
 static void AppStateManager(uint8_t event);
 static void LoadConfiguration(void);
-static void platform_init_randomizer(void);
-uint32_t RandomNumberGenerate(void);
+static uint16_t platform_init_randomizer(void);
+static void change_speed();
+
 /******************************************************************************/
 /*                            EXPORTED FUNCTIONS                              */
 /******************************************************************************/
 void DeviceStateMachine(uint8_t event);
 void SetupPrg();
+
 void platform_get_control(snake_t * snake);
+
 void snake_init(snake_t* snake);
 void snake_move(snake_t* snake);
 void snake_haseaten(snake_t* snake, food_t* food);
 void snake_display(snake_t* snake);
+void platform_eraseCell(uint16_t x, uint16_t y);
+void platform_drawCell(uint16_t x, uint16_t y);
+void snake_place_food(snake_t* snake, food_t* food, uint32_t tick);
+uint16_t generate_food(snake_t* snake, food_t *food);
+void platform_drawFood(uint16_t x, uint16_t y);
+void platform_deleteFood(uint16_t x, uint16_t y);
+void platform_fatal(void);
+void displayGameOver();
+void Score();
 /******************************************************************************/
 
 /**
@@ -134,38 +161,100 @@ void snake_display(snake_t* snake);
   * @retval None
   */
 
+void update_running(){
+	running = 1;
+}
+void TIM5config()
+{
+	RCC->APB1ENR |= (1<<3);//bật timer5 clock
+
+	TIM5->PSC = 84-1; //84MHz/84 = 1Mhz ~ 1us
+	TIM5->ARR = 0xffff;
+
+	TIM5->CR1 |= (1<<0);
+	while(!(TIM5->SR&(1<<0)));
+}
+void Delay_us(uint16_t us){
+	TIM5->CNT = 0;
+	while(TIM5->CNT < us);
+}
+void Delay_ms(uint16_t ms){
+	for(uint16_t i=0; i < ms; i++){
+		Delay_us(1000);
+	}
+}
 int main(void) {
 	AppInitCommon();
-	SetStateApp(STATE_APP_STARTUP); // Khởi động ở chế độ STATE_APP_STARTUP
-	EventSchedulerAdd(EVENT_APP_INIT); //Khởi tạo bộ đệm buffer để quản lý các sự kiện của chương trình chính
-
+	SetStateApp(STATE_APP_STARTUP); 		// Khởi động ở chế độ STATE_APP_STARTUP
+	EventSchedulerAdd(EVENT_APP_INIT); 		//Khởi tạo bộ đệm buffer để quản lý các sự kiện của chương trình chính
+	TIM5config();							//cấu hình tim5 tạo hàm delay
+	platform_init_randomizer();				//tạo gRandSeed phục vụ tạo số ngẫu nhiên
 	/* Loop forever */
 	while (1) {
+
 		processTimerScheduler(); 			// Xử lý các bộ hẹn giờ đã được tạo
 		processEventScheduler();			// Xử lý các sự kiện của chương trình chính do bộ quản lý sự kiện quản lý.
-		platform_init_randomizer();			//tạo gRandSeed
+
+		memset(score_display, 0, sizeof(score_display));
 	  	snake_t snake = { 0 };
-	  	//food_t food = { 0 };
-	  	//uint32_t gPrgCycle = 0;
+	  	food_t food = { 0 };
+	  	uint32_t gPrgCycle = 0;
 	  	snake_init(&snake);
-	  	for(;;)
-	  	{
-	  		platform_get_control(&snake);
-	  		snake_move(&snake);
-	  		if (snake.state != PLAYING) break;
+	  	uint8_t gameover = 1;
+	  	IdTimer = TimerStart("update_running", 2500, 0, update_running, NULL);
+	  	if(running){
+	  		for(;;){
+		  		platform_get_control(&snake);
+		  		snake_move(&snake);
+
+		  		if (snake.state != PLAYING){
+		  			memset(score_display, 0, sizeof(score_display));
+		  			break;}
+
+		  		snake_haseaten(&snake, &food);
+		  		snake_display(&snake);
+		  		snake_place_food(&snake, &food, gPrgCycle);
+		  		Score();
+		  		Delay_ms(speed);
+	  		}
+			while(1){
+				processTimerScheduler();
+				processEventScheduler();
+				if(gameover){
+				ucg_ClearScreen(&ucg1);
+				gameover = 0;
+				IdTimer = TimerStart("gameOver", 5, 0, displayGameOver, NULL);
+				}
+			}
 	  	}
-
-
-
+	}
+}
+static void change_speed(){
+	if(speed == SPEED_1){
+		speed = SPEED_2;
+	}else{
+		speed = SPEED_1;
 	}
 
+}
+void Score(){
+	processTimerScheduler();
+	ucg_DrawString(&ucg1, 42, 127, 0, score_display);
+
+}
+void displayGameOver()
+{
+	if(IdTimer != NO_TIMER){
+	TimerStop(IdTimer);
+	sprintf(score_display, "GameOver-%d", score);
+	}
 }
 
 
 
 static void AppInitCommon(void) {
 	SystemCoreClockUpdate(); 				//Initializes system clock để cấu hình clock của hệ thống
-	TimerInit();							// Initializes system tick để xử lý các sự kiện thời gian.
+	TimerInit();						// Initializes system tick để xử lý các sự kiện thời gian.
 
 
 	EventSchedulerInit(AppStateManager); 	// Khởi tạo bộ đệm buffer để quản lý các sự kiện của chương trình chính
@@ -179,7 +268,7 @@ static void AppInitCommon(void) {
 	Ucglib4WireSWSPI_begin(&ucg, UCG_FONT_MODE_SOLID);
 	ucg_ClearScreen(&ucg);
 	ucg_SetFont(&ucg, ucg_font_ncenR10_hr);
-	ucg_SetColor(&ucg, 0, 100, 100, 100);
+	ucg_SetColor(&ucg, 0, 255, 255, 255);
 	ucg_SetColor(&ucg, 1, 0, 0, 0);
 	ucg_SetRotate180(&ucg);
 	Ucglib4WireSWSPI_begin(&ucg1, UCG_FONT_MODE_SOLID);
@@ -187,9 +276,6 @@ static void AppInitCommon(void) {
 	ucg_SetColor(&ucg1, 0, 255, 255, 255);
 	ucg_SetColor(&ucg1, 1, 0, 0, 0);
 	ucg_SetRotate180(&ucg1);
-
-
-
 }
 
 static state_app_t GetStateApp(void) {
@@ -225,25 +311,20 @@ static void LoadConfiguration(void) {
 	ucg_DrawString(&ucg, 55, 24, 0, "IOT");
 	ucg_DrawString(&ucg, 5, 48, 0, "Programming by");
 	ucg_DrawString(&ucg, 5, 72, 0, "Lumi Smarthome");
-	IdTimer = TimerStart("ClearScreenAndSetup", 1500, 0, SetupPrg, NULL);
+	ucg_ClearScreen(&ucg);
+	TimerStart("ClearScreenAndSetup", 1200, 0, SetupPrg, NULL);
 }
 
 void SetupPrg(){
-	if(IdTimer != NO_TIMER){
 		ucg_ClearScreen(&ucg);
-		ucg_DrawFrame(&ucg1, 0, 0, 127, 117);
+		ucg_DrawFrame(&ucg1, 0, 7, 127, 112);
 		ucg_DrawString(&ucg1, 0, 127, 0, "Score: 0");
-		ucg_DrawBox(&ucg1, 20, 20, 20, 20);
-		ucg_DrawBox(&ucg, 40, 40, 20, 20);
-		TimerStop(IdTimer);
-	}
-
 }
 
 void snake_init(snake_t* snake)
 {
 	snake->length = SNAKE_INIT_LNG;
-	snake->direction = UP;
+	snake->direction = RIGHT;
 	snake->state = PLAYING;
 	snake->ghost.x = INVALID_COORDS;
 	snake->ghost.y = INVALID_COORDS;
@@ -255,18 +336,16 @@ void snake_init(snake_t* snake)
 		snake->body[idx].x = SNAKE_INIT_X_CORD + idx;
 		snake->body[idx].y = SNAKE_INIT_Y_CORD;
 	}
-
-	//platform_refresh_hw();
-    //snake_diplay_borders();
+	speed = SPEED_1;
 }
 
-//tạo gRandSeed để tạo số ngẫu nhiên cho vị trí food
-static void platform_init_randomizer(void)
+static uint16_t platform_init_randomizer(void)//tạo gRandSeed để tạo số ngẫu nhiên cho vị trí food
 {
 	/* This randomizer is based on ADC noise
 	 * as a LFSR seed number */
 	gRandSeed = LightSensor_MeasureUseDMAMode();
-	while(gRandSeed < 0x8000) gRandSeed += gRandSeed;
+	//while(gRandSeed < 0x8000) gRandSeed += gRandSeed;
+	return gRandSeed;
 }
 
 //lấy giá trị khi nhấn nút để di chuyển snake
@@ -282,9 +361,7 @@ void platform_get_control(snake_t * snake)
     if(!Button_GetLogicInputPin(BUTTON_KIT_ID1))extKeyBoardButton='W';
     if(!Button_GetLogicInputPin(BUTTON_KIT_ID4))extKeyBoardButton='D';
     if(!Button_GetLogicInputPin(BUTTON_KIT_ID2))extKeyBoardButton='A';
-
-//    if(!HAL_GPIO_ReadPin(GPIOx, GPIO_PIN_x))extKeyBoardButton='P';
-//    if(!HAL_GPIO_ReadPin(GPIOx, GPIO_PIN_x))extKeyBoardButton='Q';
+    if(!Button_GetLogicInputPin(BUTTON_KIT_ID3))extKeyBoardButton='P';
 
 	direction = (snake_dir_e)extKeyBoardButton;
 
@@ -300,23 +377,30 @@ void platform_get_control(snake_t * snake)
 	{
 		prev_direction = snake->direction;
 		snake->direction = PAUSE;
+		// nhấn nút khác các nút đã cài đặt -> lưu hướng rắn cũ, hướng rắn đặt là PAUSE
 	}
 	else
 	{
-		if (direction == PAUSE)
+		if (direction == PAUSE)//nhấn PAUSE
 		{
 			if (snake->direction != PAUSE)
 			{
 				prev_direction = snake->direction;
 				snake->direction = PAUSE;
+				change_speed();
 			}
+			//hướng rắn cũ khác PAUSE thì lưu hướng rắn cũ, hướng rắn đặt là PAUSE
+			//-> tạm dừng game
 			else
 			{
 				snake->direction = prev_direction;
 			}
+
+			//còn hướng rắn cũ khác PAUSE thì hướng rắn được lấy lại là hướng cũ
+			//-> tiếp tục game
 		}
 
-		else
+		else //nhấn một trong các nút chọn hướng
 		{
 			if ((snake->direction != PAUSE) &&
 				!(snake->direction == LEFT && direction == RIGHT) &&
@@ -326,6 +410,7 @@ void platform_get_control(snake_t * snake)
 			{
 				snake->direction = direction;
 			}
+			//loại trừ các hướng điều khiển quay đầu 180 độ thì
 		}
 	}
 }
@@ -336,15 +421,16 @@ void snake_move(snake_t* snake)
 	{
 		return;
 	}
-	//luôn lưu tọa độ đuôi để khi ăn được food sẽ sử dụng tăng độ dài
-	snake->ghost = snake->body[0];
 
-	/*di chuyển theo hướng hiện tại -> lưu length-1 tọa độ lại ngoại trừ phần đầu,
-	direction luôn đc cập nhật bởi hàm snake_control sẽ quyết định cho hướng đi
-	tiêp theo -> tạo ra tọa độ mới cho phần đầu*/
+	snake->ghost = snake->body[0];
+	//luôn lưu tọa độ đuôi để khi ăn được food sẽ sử dụng tăng độ dài
+
 	memcpy(&snake->body[0], &snake->body[1], sizeof(coord_t) * (snake->length - 1));
+	//di chuyển theo hướng hiện tại -> lưu (length-1) tọa độ lại ngoại trừ phần đầu
 
 	switch (snake->direction)
+	/*direction luôn đc cập nhật bởi hàm snake_control sẽ quyết định cho hướng đi
+	tiêp theo -> tạo ra tọa độ mới cho phần đầu*/
 	{
 	case UP:
 	{
@@ -426,7 +512,7 @@ void snake_move(snake_t* snake)
 	default:
 	{
 	}
-	//break;?
+	//nobreak;?
 	}
 
 	if (snake->length == SNAKE_WON_LIMIT)
@@ -439,54 +525,173 @@ void snake_haseaten(snake_t* snake, food_t* food)
 {
 	if ((snake->body[snake->length - 1].x == food->coord.x)
 		&& (snake->body[snake->length - 1].y == food->coord.y))
+		//đầu rắn trùng food
 	{
-		/* Needed temporary copy for shifting the whole array right - for embedded*/
-		//lưu toàn bộ tọa độ của body vào biến tạm
 		coord_t tempSnake[SNAKE_MAX_LNG] = {0};
-		memcpy(tempSnake, &(snake->body[0]), (size_t)snake->length*sizeof(coord_t));
+		memcpy(tempSnake, &(snake->body[0]), snake->length*sizeof(coord_t));
+		//lưu toàn bộ tọa độ của body vào biến tạm
 
-		/* Just append the ghost to the end, increment length and disable ghost*/
+		memcpy(&(snake->body[1]), tempSnake, snake->length*sizeof(coord_t));
 		//dịch toàn bộ tọa độ body sang 1 body
-		memcpy(&(snake->body[1]), tempSnake, (size_t)snake->length*sizeof(coord_t));
-		snake->body[0] = snake->ghost; //lấy tọa độ mới cho đuôi vì ghost luôn đc cập nhật bởi hàm snake_move()
+
+		snake->body[0] = snake->ghost;
+		//lấy tọa độ mới cho đuôi vì ghost luôn đc cập nhật bởi hàm snake_move()
+		platform_deleteFood(food->coord.x, food->coord.y);
+		//xóa food cũ
+
 		snake->ghost.x = INVALID_COORDS;
 		snake->ghost.y = INVALID_COORDS;
-		snake->length++;//sau ăn thì xóa ghost và tăng độ dài
-
+		snake->length++;
+		//sau ăn thì xóa ghost và tăng độ dài
 		food->state = EATEN;
+		score++;
+		memset(score_display, 0, sizeof(score_display));//khởi tạo lại giá trị cho score_display
+		sprintf(score_display, "%d", score);
 	}
 }
 
-//void snake_display(snake_t* snake)
-//{
-//	if (INVALID_COORDS != snake->ghost.x && INVALID_COORDS != snake->ghost.y)
-//	{
-//		platform_eraseCell(snake->ghost.x, snake->ghost.y);
-//	}
-//	for (int idx = 0; idx < snake->length; idx++)
-//	{
-//		platform_drawCell(snake->body[idx].x, snake->body[idx].y);
-//	}
-//}
-
-uint32_t RandomNumberGenerate(void)
+void snake_display(snake_t* snake)
 {
-	/* Wait until one RNG number is ready */
-	while(RNG_GetFlagStatus(RNG_FLAG_DRDY)== RESET)
+	if (INVALID_COORDS != snake->ghost.x && INVALID_COORDS != snake->ghost.y)
 	{
+		platform_eraseCell(snake->ghost.x, snake->ghost.y);//hàm xóa 1 Cell body của snake
 	}
+	for (int idx = 0; idx < snake->length; idx++)
+	{
 
-	/* Get a 32bit Random number */
-	return RNG_GetRandomNumber();
+		platform_drawCell(snake->body[idx].x, snake->body[idx].y);//hàm vẽ 1 Cell body của snake
+	}
+}
+
+void platform_drawCell(uint16_t x, uint16_t y)
+{
+	uint16_t x_new= ARENA_OFFSET_X + x*CELL_SIZE;//tọa độ x của cell snake
+	uint16_t y_new= ARENA_OFFSET_Y + y*CELL_SIZE;//tọa độ y của cell snake
+	for(uint8_t idx = x_new; idx < x_new + CELL_SIZE; idx++){
+		ucg_DrawPixel(&ucg1, idx, y_new);
+	}
+	for(uint8_t idx = y_new; idx < y_new + CELL_SIZE; idx++){
+		ucg_DrawPixel(&ucg1, x_new, idx);
+	}
+	for(uint8_t idx = x_new; idx < x_new + CELL_SIZE; idx++){
+		ucg_DrawPixel(&ucg1, idx, y_new + CELL_SIZE-1);
+	}
+	for(uint8_t idx = y_new; idx < y_new + CELL_SIZE; idx++){
+		ucg_DrawPixel(&ucg1, x_new + CELL_SIZE - 1, idx);
+	}
+}
+
+void platform_eraseCell(uint16_t x, uint16_t y)
+{
+	uint16_t x_new= ARENA_OFFSET_X + x*CELL_SIZE;//tọa độ x đặt cell snake
+	uint16_t y_new= ARENA_OFFSET_Y + y*CELL_SIZE;//tọa độ y đặt cell snake
+	ucg_SetColor(&ucg1, 0, 0, 0, 0);
+	for(uint8_t idx = x_new; idx < x_new + CELL_SIZE; idx++){
+		ucg_DrawPixel(&ucg1, idx, y_new);
+	}
+	for(uint8_t idx = y_new; idx < y_new + CELL_SIZE; idx++){
+		ucg_DrawPixel(&ucg1, x_new, idx);
+	}
+	for(uint8_t idx = x_new; idx < x_new + CELL_SIZE; idx++){
+		ucg_DrawPixel(&ucg1, idx, y_new + CELL_SIZE-1);
+	}
+	for(uint8_t idx = y_new; idx < y_new + CELL_SIZE; idx++){
+		ucg_DrawPixel(&ucg1, x_new + CELL_SIZE - 1, idx);
+	}
+	ucg_SetColor(&ucg1, 0, 255, 255, 255);
+}
+
+void platform_fatal(void)
+{
+	while (1){
+		processTimerScheduler();
+		ucg_DrawString(&ucg1, 50, 50, 0, "ERROR PROG");
+	};
+}
+
+void snake_place_food(snake_t* snake, food_t* food, uint32_t tick)
+{
+	if (0 == gPrgCycle % 10 || food->time_elapsed)
+	{
+		if (food->state != PLACED)
+		{
+			if (GENERAL_ERROR == generate_food(snake, food))
+			{
+				platform_fatal();
+			}
+			else
+			{
+				food->time_elapsed = 0;
+				food->state = PLACED;
+			}
+		}
+		else
+		{
+			food->time_elapsed = 1;
+		}
+	}
+}
+
+uint16_t generate_food(snake_t* snake, food_t *food)
+{
+	uint16_t isInvalid = 0;
+	uint16_t iter = 0;
+
+	do
+	{
+		food->coord.x = (uint16_t)(((platform_init_randomizer()) % (FOOD_MAX_X - FOOD_MIN_X + 1)) + FOOD_MIN_X);
+		food->coord.y = (uint16_t)(((platform_init_randomizer())% (FOOD_MAX_Y - FOOD_MIN_Y )) + FOOD_MIN_Y);
+		//platform_randomize()
+		for (int idx = 0; idx < snake->length; idx++)
+		{
+			if ((snake->body[idx].x == food->coord.x) &&
+				(snake->body[idx].y == food->coord.y))
+			{
+				isInvalid = GENERAL_ERROR;
+				break;
+			}//else{isInvalid = 0;}
+		}
+		iter++;
+		if (iter > FOOD_MAX_ITER)
+		{
+			break;
+		}
+	} while (isInvalid);
+
+	if (!isInvalid)
+	{
+		platform_drawFood(food->coord.x, food->coord.y);//,
+	}
+	return isInvalid;
+}
+
+void platform_drawFood(uint16_t x, uint16_t y)
+{
+	uint16_t x_new= ARENA_OFFSET_X + x*CELL_SIZE + 1;//tọa độ x đặt food
+	uint16_t y_new= ARENA_OFFSET_Y + y*CELL_SIZE + 1;//tọa độ y đặt food
+	for(uint8_t idx_i = x_new; idx_i < x_new + FOOD_SIZE; idx_i++){
+		for(uint8_t idx_j = y_new; idx_j < y_new + FOOD_SIZE; idx_j++)
+		{
+			ucg_DrawPixel(&ucg1, idx_i, idx_j);
+		}
+	}
+}
+void platform_deleteFood(uint16_t x, uint16_t y)
+{
+	ucg_SetColor(&ucg1, 0, 0, 0, 0);
+	uint16_t x_new= ARENA_OFFSET_X + x*CELL_SIZE + 1;//tọa độ x đặt food
+	uint16_t y_new= ARENA_OFFSET_Y + y*CELL_SIZE + 1;//tọa độ y đặt food
+	for(uint8_t idx_i = x_new; idx_i < x_new + FOOD_SIZE; idx_i++){
+		for(uint8_t idx_j = y_new; idx_j < y_new + FOOD_SIZE; idx_j++)
+		{
+			ucg_DrawPixel(&ucg1, idx_i, idx_j);
+		}
+	}
+	ucg_SetColor(&ucg1, 0, 255, 255, 255);
 }
 
 void DeviceStateMachine(uint8_t event){
 	switch (event) {
-		case EVENT_OF_BUTTON_1_PRESS_LOGIC:
-		{
-
-		}
-			break;
 		default:
 			break;
 	}
